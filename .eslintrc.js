9f82fdd4c6c6a53f248b7fc64f3c641c75f233c4
joi0104 <jjjjoi0104@gmail.com>
2021-02-26 21:35:05 +0900

    chore: 개발환경 셋팅

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: ['react-app', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'simple-import-sort'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# Mash-Up open api 세미나
+
+1. 미리 쓸만한 open API를 2~3개 선정
+2. 2~3명씩 팀을 구성
+3. 2시간동안 open API를 사용하여 자유주제로 토이프로젝트 진행
+4. 1시간동안 서로 어떤 프로젝트를 작성했는지 소개 및 리뷰
+
+## 참고사이트
+[https://github.com/dl0312/open-apis-korea](https://github.com/dl0312/open-apis-korea) - 다양한 카테고리가 장점! 한국인이 작성한 문서라 문서가 한국어라 사용하기 편리함
+[https://english.api.rakuten.net](https://english.api.rakuten.net) - API계의 큰손 라쿠텐 api. 유용한 API들을 판매하는 마켓플레이스로 유용한 api들이 많고 유료인만큼 사용법이나 예시코드가 잘되있음. 일정 사용량부터 과금되서 토이프로젝트용으로는 무료로 사용할 수 있는 api 많음
