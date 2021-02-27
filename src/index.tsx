7ec8baf2b5c48f76c60533d50a60ecd063dd0b66
ifmoon <ifmoon.io@gmail.com>
2021-02-27 17:28:52 +0900

    implement router

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import 'sanitize.css/sanitize.css';
 
-/* Internal dependencies */
-import App from 'App';
 import store from 'features';
 /* External dependencies */
 import React from 'react';
@@ -11,6 +9,9 @@ import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import * as serviceWorker from 'serviceWorker';
+
+/* Internal dependencies */
+import App from './routes';
 
 ReactDOM.render(
   <Provider store={store}>
